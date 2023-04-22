95c84c0cedd94c7b0360b1ac219efc70098f3eb5
strawberryhacker <bjorn.thoralf@outlook.com>
2023-04-22 10:22:46 +0200

    Update

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, April 2023
+//  
+// Copyright (C) 2024 strawberryhacker
+// 
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//  
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+// 
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 #include <stdlib.h>
 #include <assert.h>
 #include <stdio.h>
